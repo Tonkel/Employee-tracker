7f585292b7001c456fc0fbbb00e357275013adce
Tonkel <w.tucker.cac@gmail.com>
2023-10-14 17:48:55 -0400

    adds front end fetch functions, and adheres them to the inquirer prompt response

--- main/index.js
+++ main/index.js
@@ -22,14 +22,35 @@ function init() {
     ])
     .then((response) => {
       console.log(response);
-      getEmployees();
+      if (response.choice === "view all departments") {
+        //get departments then rerun prompt
+        getDepartments();
+        //wait a second before prompting again
+        setTimeout(function () {
+          init();
+        }, 1000);
+      } else if (response.choice === "view all roles") {
+        //get roles then rerun prompt after a second
+        getRoles();
+        //wait a second before prompting again
+        setTimeout(function () {
+          init();
+        }, 1000);
+      } else if (response.choice === "view all employees") {
+        //get roles then rerun prompt after a second
+        getEmployees();
+        //wait a second before prompting again
+        setTimeout(function () {
+          init();
+        }, 1000);
+      }
     });
 }
 
-// calls function
-init();
+//add a department inquirer prompt
 
-const getEmployees = async () => {
+//fetch functions
+const getDepartments = async () => {
   //fetch data
   const response = await fetch("http://localhost:3001/api/view-departments", {
     method: "GET",
@@ -38,8 +59,8 @@ const getEmployees = async () => {
 
   if (response.ok) {
     const data = await response.json();
-
-    console.log(`id department \n-- -----------`);
+    //custom display
+    console.log(`\n id department \n-- ----------`);
     //loop through data to log it
     for (let i = 0; i < data.length; i++) {
       console.log(`${data[i].id}  ${data[i].department}`);
@@ -48,3 +69,54 @@ const getEmployees = async () => {
     console.log(response.statusText);
   }
 };
+
+const getEmployees = async () => {
+  //fetch data
+  const response = await fetch("http://localhost:3001/api/view-employees", {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    //custom display
+    console.log(
+      `id first_name last_name   title      department      salary      manager \n-- ---------- ---------  ---------   -----------    -------     -------------`
+    );
+    //loop through data to log it
+    for (let i = 0; i < data.length; i++) {
+      console.log(
+        `${data[i].id}  ${data[i].first_name}      ${data[i].last_name}        ${data[i].title}        ${data[i].department}        ${data[i].salary}      ${data[i].manager}`
+      );
+    }
+  } else {
+    console.log(response.statusText);
+  }
+};
+
+const getRoles = async () => {
+  //fetch data
+  const response = await fetch("http://localhost:3001/api/view-roles", {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    //custom display
+    console.log(
+      `id title      department       salary \n-- --------   -------------    -----------`
+    );
+    //loop through data to log it
+    for (let i = 0; i < data.length; i++) {
+      console.log(
+        `${data[i].id}  ${data[i].title}         ${data[i].department}         ${data[i].salary}`
+      );
+    }
+  } else {
+    console.log(response.statusText);
+  }
+};
+
+// calls function
+init();
